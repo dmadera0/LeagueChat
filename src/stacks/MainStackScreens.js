23c194b3b21f25a665030514cb180931a0f9e441
Daniel <dmadera0@gmail.com>
2020-11-05 11:11:10 -0700

    added icons

--- src/stacks/MainStackScreens.js
+++ src/stacks/MainStackScreens.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
+import { Ionicons } from "@expo/vector-icons";
 
 
 import HomeScreen from "../screens/HomeScreen";
@@ -13,8 +13,49 @@ import ProfileScreen from "../screens/ProfileScreen";
 export default MainStackScreens = () => {
     const MainStack = createBottomTabNavigator()
 
+    const tabBarOptions = {
+        showLabel: false,
+        style: {
+            backgroundColor: "#8022d9",
+            paddingBototm: 12,
+        },
+    };
+
+    const screenOptions = ({route}) => ({
+        tabBarIcon: ({ focused }) => {
+            let iconName = "ios-home"
+
+            switch (route.name){
+                case "Home":
+                    iconName = "ios-home";
+                    break;
+
+                case "Message":
+                    iconName = "ios-chatboxes";
+                    break;
+
+                case "Notification":
+                    iconName = "ios-notifications";
+                    break;
+
+                case "Profile":
+                    iconName = "ios-person";
+                    break;
+
+                default:
+                    iconName = "ios-home";
+
+            }
+
+            return <Ionicons name={iconName} size={24} color={focused ? "#ffffff" : "#666666"} />;
+
+        }
+    })
+
+    
+
     return (
-        <MainStack.Navigator >
+        <MainStack.Navigator tabBarOptions={tabBarOptions} screenOptions={screenOptions}>
             <MainStack.Screen name="Home" component={HomeScreen} />
             <MainStack.Screen name="Message" component={MessageScreen} />
             <MainStack.Screen name="Post" component={PostScreen} />
