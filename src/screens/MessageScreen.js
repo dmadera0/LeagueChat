c12718771a4f85f2d4a5ac38ee0b1ba0f8010ea5
Daniel <dmadera0@gmail.com>
2020-11-06 14:15:58 -0700

    added firebase

--- src/screens/MessageScreen.js
+++ src/screens/MessageScreen.js
@@ -7,8 +7,15 @@ import * as firebase from 'firebase'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-    //Your firebase config here
-}
+        apiKey: "AIzaSyDWs6eZsE_cu9djpJurQvA1ouxIsx5CJIg",
+        authDomain: "capstone-leaguechat-app.firebaseapp.com",
+        databaseURL: "https://capstone-leaguechat-app.firebaseio.com",
+        projectId: "capstone-leaguechat-app",
+        storageBucket: "capstone-leaguechat-app.appspot.com",
+        messagingSenderId: "177537118971",
+        appId: "1:177537118971:web:8ff06c95e74151ffd8365e"
+    };
+      // Initialize Firebas
 
 if (firebase.apps.length === 0) {
     firebase.initializeApp(firebaseConfig)
