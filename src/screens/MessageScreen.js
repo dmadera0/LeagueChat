70f49609b98187325416d14d35aad9fc4a4e9541
Daniel <dmadera0@gmail.com>
2020-11-11 11:35:25 -0700

    anouncement page restyle

--- src/screens/MessageScreen.js
+++ src/screens/MessageScreen.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect, useCallback } from 'react'
 import { GiftedChat } from 'react-native-gifted-chat'
 import AsyncStorage from '@react-native-community/async-storage'
 import { StyleSheet, TextInput, View, YellowBox, Button } from 'react-native'
+import styled from "styled-components";
 import * as firebase from 'firebase'
 import 'firebase/firestore'
 
@@ -78,6 +79,11 @@ export default function App() {
             <View style={styles.container}>
                 <TextInput style={styles.input} placeholder="Enter your name" value={name} onChangeText={setName} />
                 <Button onPress={handlePress} title="Enter the chat" />
+                <HeaderGraphic>
+               <RightCircle/>
+               <LeftCircle/>
+           </HeaderGraphic>
+
             </View>
         )
     }
@@ -102,3 +108,30 @@ const styles = StyleSheet.create({
         borderColor: 'gray',
     },
 })
+
+const RightCircle = styled.View`
+    background-color: #8022d9;
+    position: absolute;
+    width: 400px;
+    height: 400px;
+    border-radius: 200px;
+    right: -100px;
+    top: -200px;
+`;
+
+const LeftCircle = styled.View`
+    background-color: #23a6d5;
+    position: absolute;
+    width: 200px;
+    height: 200px;
+    border-radius: 100px;
+    left: -50px;
+    top: -50px;
+`;
+
+const HeaderGraphic = styled.View`
+    position: absolute;
+    width: 100%;
+    top: -50px;
+    z-index: -100;
+`;
